230fac639cf5cb11dbb32c4bed483493e13c3292
ryoshindo <udoryo999@gmail.com>
2025-03-22 11:19:13 +0900

    chore: re-init

--- apps/chat/src/app/api/chat/route.ts
+++
@@ -1,9 +0,0 @@
-import { mastra } from "../../../../../agent/src/mastra";
-
-export async function POST(req: Request) {
-	const { messages } = await req.json();
-	const myAgent = mastra.getAgent("weatherAgent");
-	const stream = await myAgent.stream(messages);
-
-	return stream.toDataStreamResponse();
-}
